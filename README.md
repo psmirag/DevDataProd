489e151251d486a9765ad41aec0ea80ea21c88c1
Pablo Smiraglia <psmiraglia@gmail.com>
2015-12-09 00:22:55 -0500

    Typo

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 Developing Data Products
 Project: FIFA Worldcup 2014 players Database
 
-You can use this simple app to find any player that participated in the 2014 FIFA World Cup. Tree Search parameters can be used:
+You can use this simple app to find any player that participated in the 2014 FIFA World Cup. Three Search parameters can be used:
 
 - Team name (eg. Argentina). Default = all
 - Position (eg. GoalKeeper). Default = any
--- ui.R
+++ ui.R
@@ -9,6 +9,13 @@ shinyUI(pageWithSidebar(
                 ),
         mainPanel(
                 h3("Players"),
+                h5("You can use this simple app to find any player that participated in the 2014 FIFA World Cup. Three Search parameters can be used:
+
+                   - Team name (eg. Argentina). Default = all
+                   - Position (eg. GoalKeeper). Default = any
+                   - Captain (each team has only one captain). Default = No
+                   
+                   Parameters can be combined to create different criteria"),
                 verbatimTextOutput("players")
         )
 ))
