39cd9612ab339f098de63c17bf899d73401927c6
Pablo Smiraglia <psmiraglia@gmail.com>
2015-12-09 00:33:57 -0500

    formatting

--- ui.R
+++ ui.R
@@ -7,11 +7,11 @@ shinyUI(pageWithSidebar(
                 radioButtons("pos", "Position:", c("GoalKeeper" = "GK", "Defender" = "DF", "Mid-fielder" = "MF", "Forward" = "FW", "Any" = "ANY"), selected = "ANY"),
                 radioButtons("cap", "Captain?", c("Yes" = "TRUE", "No" = "FALSE"), selected = "FALSE"),
                 h4 ("Instructions:"),
-                h5 ("You can use this simple app to find any player that participated in the 2014 FIFA World Cup. Three Search parameters can be used:
-                   - Team name (eg. Argentina). Default = all
-                   - Position (eg. GoalKeeper). Default = any
-                   - Captain (each team has only one captain). Default = No
-                   Parameters can be combined to create different criteria")
+                h5 ("You can use this simple app to find any player that participated in the 2014 FIFA World Cup. Three Search parameters can be used:"),
+                h5 ("- Team name (eg. Argentina). Default = all"),   
+                h5 ("- Position (eg. GoalKeeper). Default = any"),
+                h5 ("- Captain (each team has only one captain). Default = No"),
+                h5 ("Parameters can be combined to create different criteria")
                 ),
         mainPanel(
                 h3("Players"),
